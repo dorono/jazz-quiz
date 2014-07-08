d54d141bbd89aded49da12ead4fe131b0bb7d268
Doron <me@doronorenstein.com>
2014-07-07 23:00:04 -0700

    set up simple interface for instantiating quiz

--- js/lib/quiz.js
+++ js/lib/quiz.js
@@ -1,30 +1,43 @@
-var Quiz = Quiz || {};
-
-$(document).ready(function () {
-
-  // create the flash cards and display them one at a time with questions
-  // coming in via JSON
-  Quiz.getCards = function(jsonSrc) {
-      $('next , result').hide();
-      $.ajax({
-          url: jsonSrc,
-          dataType: 'json',
-          success: function (data) {
-              Quiz.buildCards(data);
-          }
-      });
-  }
-
-  Quiz.buildCards = function(jsonData) {
+function quiz(settings) {
+    var jsonSrc = settings.jsonSrc || '',
+        correctMsg = settings.correctMsg || 'Correct!',
+        incorrectMsg = settings.incorrectMsg || 'Incorrect!',
+        successMessageUnder26 = settings.successMessageUnder26 || 'Disaster!',
+        successMessageUnder50 = settings.successMessageUnder50 || 'Not too great...',
+        successMessageUnder75 = settings.successMessageUnder75 || 'Very nice!',
+        successMessageOver75 = settings.successMessageOver75 || 'Great!',
+        successMessage100 = settings.successMessage100 || 'Perfect!',
+        jsonData, 
+        questionIndex = 0,
+        totalScore = 0,
+        numQuestions, 
+        correctAns, 
+        percentage, 
+        successMessage;
+    
+    // create the flash cards and display them one at a time with questions
+    // coming in via JSON
+    this.getCards = function() {
+        $('next , result').hide();
+        $.ajax({
+            url: jsonSrc,
+            dataType: 'json',
+            success: function(data) {
+                buildCards(data);
+            }
+        });
+    }
+    
+    var buildCards = function(jsonData) {
       // we'll need these two variables outside of this function
       numQuestions = jsonData.quizQuestions.length;
       correctAns = jsonData.quizQuestions[questionIndex].correctAnswer;
-
+    
       // cute transition to get us between cards
       $('.flash-card').toggle("drop" , function() {
           $(this).remove();
       });
-
+    
       // build the flash card
       $('.container-fluid').prepend(
           '<div class="flash-card">' +
@@ -33,7 +46,7 @@ $(document).ready(function () {
           '</dl>' +
           '</div>'
       );
-
+    
       // generate answer option list for each question
       $.each(jsonData.quizQuestions[questionIndex].answers , function(x,indivAnswer) {
           $('.flash-card dl').append(
@@ -45,96 +58,93 @@ $(document).ready(function () {
           '</dd>'
           );
       });
-
+    
       // disable the prop button since the user still hasn't made a selection
       $('button').prop('disabled', true);
-
-  };
-
-  // Handles the scoring and messaging upon the user's answer input
-  Quiz.submitAnswer = function(correctMsg,incorrectMsg) {
-      // if the answer is correct
-      if($('input[type="radio"]:checked').val() == correctAns) {
-          totalScore++;
-          $('score span').text(totalScore);
-          $('result').removeClass('incorrect').addClass('correct').text(correctMsg);
-      } else {
-      // if the answer is incorrect
-          $('.flash-card').effect('shake');
-          $('result').removeClass('correct').addClass('incorrect').text(incorrectMsg);
-          $('input[value="' + correctAns+ '"]').val(correctAns).parent('label').addClass('highlighted');
-      }
-
-      // once an answer has been submitted, disable the radio and submission buttons
-      $('button , input').prop('disabled', true);
-      $('result').show();
-      if((questionIndex + 1) < numQuestions) {
-          $('next').show();
-      } else {
-          // if this is the very last question, give it a few seconds and then display
-          // the results screen
-          setTimeout(function() {
-              Quiz.tallyResults()
-          } , 2000);
-      }
-      return;
-  }
-
-  // handles the computation of the score as well as displaying
-  // the final summary screen
-  Quiz.tallyResults = function() {
-      $('.flash-card , button , result').remove();
-      $('#total').show();
-      $('#total h3 strong').text(totalScore);
-      if(totalScore == 1) {
-          $('#total plural').hide();
-      }
-      $('#total h3 span').text(numQuestions);
-      Quiz.determinePercentage(successMessageUnder26 , successMessageUnder50 , successMessageUnder75 , successMessageOver75 , successMessage100);
-      $('#total h4').html(successMessage);
-      $('score').hide();
-  }
-
-  // determine the percentage of correct answers, and display appropriate
-  // text summarizing the user's performance
-  Quiz.determinePercentage =
-  function(successMessageUnder26 , successMessageUnder50 , successMessageUnder75 , successMessageOver75 , successMessage100) {
-      percentage = Math.floor(totalScore/numQuestions * 100);
-      $('percentage').text(percentage + "%");
-      if(percentage < 25) {
-          successMessage = successMessageUnder26;
-      } else if(percentage >= 25 && percentage < 50) {
-          successMessage = successMessageUnder50;
-      } else if(percentage >= 50 && percentage < 75) {
-          successMessage = successMessageUnder75;
-      } else if(percentage >= 75 && percentage < 100) {
-          successMessage = successMessageOver75;
-      } else {
-          successMessage = successMessage100;
-      }
-
-  }
-
-  // because the radio buttons are coming in via AJAX, we must wait until the AJAX
-  // call is complete before assigning any sort of click handler to these
-  $(document).ajaxSuccess(function() {
-      $('input').on('click' , function() {
-          $('button').prop('disabled', false);
-      })
-  });
-
-  $('button').click(function() {
-      Quiz.submitAnswer(correctMsg,incorrectMsg);
-  });
-
-  $('a.continue').click(function(e) {
-      e.preventDefault();
-      if((questionIndex + 1) >= numQuestions) {
-          $('button').prop('disabled', true);
-      } else {
-          questionIndex++;
-          Quiz.getCards(jsonSrc);
-      }
-  });
-
-});
+    
+    };
+    // Handles the scoring and messaging upon the user's answer input
+    var submitAnswer = function() {
+        // if the answer is correct
+        if ($('input[type="radio"]:checked').val() == correctAns) {
+            totalScore++;
+            $('score span').text(totalScore);
+            $('result').removeClass('incorrect').addClass('correct').text(correctMsg);
+        } else {
+            // if the answer is incorrect
+            $('.flash-card').effect('shake');
+            $('result').removeClass('correct').addClass('incorrect').text(incorrectMsg);
+            $('input[value="' + correctAns + '"]').val(correctAns).parent('label').addClass('highlighted');
+        }
+        // once an answer has been submitted, disable the radio and submission buttons
+        $('button , input').prop('disabled', true);
+        $('result').show();
+        if ((questionIndex + 1) < numQuestions) {
+            $('next').show();
+        } else {
+            // if this is the very last question, give it a few seconds and then display
+            // the results screen
+            setTimeout(function() {
+                tallyResults()
+            }, 2000);
+        }
+        return;
+    }
+    
+    // handles the computation of the score as well as displaying
+    // the final summary screen
+    var tallyResults = function() {
+        $('.flash-card , button , result').remove();
+        $('#total').show();
+        $('#total h3 strong').text(totalScore);
+        if (totalScore == 1) {
+            $('#total plural').hide();
+        }
+        $('#total h3 span').text(numQuestions);
+        determinePercentage();
+        $('#total h4').html(successMessage);
+        $('score').hide();
+    }
+    
+    // determine the percentage of correct answers, and display appropriate
+    // text summarizing the user's performance
+    var determinePercentage = function() {
+        percentage = Math.floor(totalScore / numQuestions * 100);
+        $('percentage').text(percentage + "%");
+        if (percentage < 25) {
+            successMessage = successMessageUnder26;
+        } else if (percentage >= 25 && percentage < 50) {
+            successMessage = successMessageUnder50;
+        } else if (percentage >= 50 && percentage < 75) {
+            successMessage = successMessageUnder75;
+        } else if (percentage >= 75 && percentage < 100) {
+            successMessage = successMessageOver75;
+        } else {
+            successMessage = successMessage100;
+        }
+        console.log(successMessage100);
+    }
+    
+    // because the radio buttons are coming in via AJAX, we must wait until the AJAX
+    // call is complete before assigning any sort of click handler to these
+    $(document).ajaxSuccess(function() {
+        $('input').on('click', function() {
+            $('button').prop('disabled', false);
+        })
+    });
+    
+    $('button').click(function() {
+        submitAnswer();
+    });
+    
+    getNextCard = this.getCards;
+    $('a.continue').click(function(e) {
+        e.preventDefault();
+        if ((questionIndex + 1) >= numQuestions) {
+            $('button').prop('disabled', true);
+        } else {
+            questionIndex++;
+            getNextCard(jsonData);
+        }
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
-var jsonSrc = 'json/questions.json',
-correctMsg = 'Woo-hoo, correct!',
-incorrectMsg = 'Oops-a-daisy, incorrect!',
-successMessageUnder26 = 'Hmm...yeah, ok. Do you even know what jazz <em>is</em>?',
-successMessageUnder50 = 'OK, I guess you know <em>something</em>, but you really should check out some more stuff.',
-successMessageUnder75 = "Alright, we're getting somewhere. Keep at it!",
-successMessageOver75 = 'Well done, my friend, you are definitely <em>not</em> jive.',
-successMessage100 = 'OOH-BOP SHA-BAM, a perfect performance!',
-jsonData,
-questionIndex = 0,
-totalScore = 0,
-numQuestions,
-correctAns,
-percentage,
-successMessage;
-
 $(document).ready(function () {
-
-  // load up the first question upon page load
-  Quiz.getCards(jsonSrc);
-
+    // instantiate the quiz with custom options
+    var jazzQuiz = new quiz({
+        jsonSrc : '/jazz-quiz/json/questions.json',
+        correctMsg : 'Woo-hoo, correct!',
+        incorrectMsg : 'Oops-a-daisy, incorrect!',
+        successMessageUnder26 : 'Hmm...yeah, ok. Do you even know what jazz <em>is</em>?',
+        successMessageUnder50 : 'OK, I guess you know <em>something</em>, but you really should check out some more stuff.',
+        successMessageUnder75 : "Alright, we're getting somewhere. Keep at it!",
+        successMessageOver75 : 'Well done, my friend, you are definitely <em>not</em> jive.',
+        successMessage100 : 'OOH-BOP SHA-BAM, a perfect performance!'
+    })  	
+    // start the quiz!
+    jazzQuiz.getCards();
 });
